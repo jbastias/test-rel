4a19787ffdf8f3000de8193380493f0307feaedd
jbastias <jorgebastias@hotmail.com>
2019-12-16 10:55:50 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import '@babel/polyfill';
+import yargs from 'yargs';
+
+const argv = yargs
+  .option('foobar', {
+    alias: 'f',
+    describe: 'foobar',
+    type: 'string',
+    // require: true,
+    default: null,
+  })
+  .help()
+  .version().argv;
+
+const run = ({ foobar }) => {
+  console.log(foobar || 'xxx');
+};
+
+(async () => {
+  let options = null;
+  if (argv.foobar) {
+    options = { foobar: argv.foobar };
+  }
+
+  await run(options || {});
+})();
