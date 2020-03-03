19ec4ce48905d7704efd160312aaf3b8c1ff46f1
jbastias <jorgebastias@hotmail.com>
2020-03-02 21:30:38 -0600

    feat: new version please 3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 export const run = ({ foobar = 'test' }) => {
-  console.log('testing 5 comment on different branch');
+  console.log('testing 6 comment on different branch');
   return foobar;
 };
 
 export const bad = ({ foobar = 'test' }) => {
-  console.log('testing 5 BAD comment on different branch');
+  console.log('testing 6 BAD comment on different branch');
   return foobar;
 };
 
 export const good = ({ foobar = 'test' }) => {
-  console.log('testing 5 GOOD comment on different branch');
+  console.log('testing 6 GOOD comment on different branch');
   return foobar;
 };
 
 export const ugly = ({ foobar = 'test' }) => {
-  console.log('testing 5 UGLY comment on different branch');
+  console.log('testing 6 UGLY comment on different branch');
   return foobar;
 };
