37e744e4beeeda780ea6e86b2fb2df81c9c941b2
jbastias <jorgebastias@hotmail.com>
2020-03-04 10:17:03 -0600

    feat: new version please 23

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export const run = ({ foobar = 'test' }) => {
-  console.log('testing 26 comment on different branch');
+  console.log('testing 27 comment on different branch');
   return foobar;
 };
 
