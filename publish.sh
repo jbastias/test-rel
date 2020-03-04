b7e490ed63705df4ce2cce5763632c44024c140a
jbastias <jorgebastias@hotmail.com>
2020-03-04 10:29:02 -0600

    feat: new version please 26

--- publish.sh
+++ publish.sh
@@ -2,30 +2,17 @@
 
 # set -eo pipefail
 
-VERSION="v$1" && \
-echo "ver: $VERSION"
+VERSION="$1" && \
+TAG="v$1" && \
+echo "ver: $VERSION $TAG"
 
 # npm publish --tag $VERSION
-
-echo "$@"
 
 ls me-so-solly
 
 if [ "$?" != "0" ]; then
-  git tag -d $VERSION && \
-  git push --delete origin $VERSION
+  git tag -d $TAG && \
+  git push --delete origin $TAG
   echo "BAD";
   exit 1;
-else
-  echo "GOOD";
 fi
-
-
-# rm -rf ~/.ssh > /dev/null
-
-# mkdir ~/.ssh/ && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
-
-# git tag -d $VERSION && \
-# git push --delete origin $VERSION
-
-# exit 1
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export const run = ({ foobar = 'test' }) => {
-  console.log('testing 29 comment on different branch');
+  console.log('testing 30 comment on different branch');
   return foobar;
 };
 
