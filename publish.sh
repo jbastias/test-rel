a20b0e2f24a7368bef7ca149bed0756625974a31
jbastias <jorgebastias@hotmail.com>
2020-03-04 10:33:44 -0600

    feat: new version please 29

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# set -eo pipefail
-
 VERSION="$1" && \
 TAG="v$1" && \
 echo "ver: $VERSION $TAG"
@@ -11,8 +9,8 @@ echo "ver: $VERSION $TAG"
 ls me-so-solly
 
 if [ "$?" != "0" ]; then
+  echo "failed to deploy, removing the already created tag";
   git tag -d $TAG && \
   git push --delete origin $TAG
-  echo "BAD";
   exit 1;
 fi
