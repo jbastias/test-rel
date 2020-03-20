93257970a166489cb86205be3f05eba5a555e732
jbastias <jorgebastias@hotmail.com>
2020-03-20 14:09:40 -0600

    update publish

--- publish.sh
+++ publish.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
 VERSION="$1" && \
-TAG="v$1" && \
-echo "ver: $VERSION $TAG"
+TAG="v$1"
 
 # npm publish --tag $VERSION
 
 ls && ls me-so-solly
 
 if [ "$?" != "0" ]; then
-  echo "failed to deploy";
+  echo "failed to deploy...";
 
   mkdir ~/.ssh/ && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 
   git fetch --all
   GITHUB_TAG=`git --no-pager tag | grep $TAG`
 
-  echo $GITHUB_TAG
-
   if [ "$GITHUB_TAG" != "" ]; then
-    echo "Removing the already created tag: $TAG";
+    echo "Removing the already created tag from github: $TAG";
     git tag -d $TAG && \
     git push --delete origin $TAG
     exit 1;
